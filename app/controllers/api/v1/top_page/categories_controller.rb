4540d9d686871928a4a0d97904269abf04a4be09
yongsokheng <yongsokheng@ymail.com>
2017-06-27 17:34:13 +0700

    Fix api top page

--- app/controllers/api/v1/top_page/categories_controller.rb
+++ app/controllers/api/v1/top_page/categories_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::TopPage::CategoriesController < ApiController
     categories = Category.order_sort.includes :published_articles
     render json: {
       success: true,
-      data: ActiveModel::Serializer::CollectionSerializer.new(
+      categories: ActiveModel::Serializer::CollectionSerializer.new(
         categories, serializer: Api::V1::TopPage::CategorySerializer
       )
     }
